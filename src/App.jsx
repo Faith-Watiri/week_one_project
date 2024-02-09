a4d1155fc177147fa4031a968f5883ebc3c4c7bc
Faith-Watiri <watiri216@gmail.com>
2024-02-09 05:45:08 +0300

    commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ThreeColumnCard from './Components/ThreeColumnCard';
+
+function App() {
+    return ( 
+
+        <div className ="App" >
+            <ThreeColumnCard />
+
+        </div>
+    );
+}
+
+export default App;
---
+++ src/ThreeColumnCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ThreeColumnCard.css';
+
+
+const ThreeColumnCard = () => {
+    return ( <div className ="three-column-card">
+
+        </div>
+    );
+};
+
+export default ThreeColumnCard;
